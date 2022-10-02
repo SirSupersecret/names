fe48d56acc0706348b20856b8e78c39e304bc782
F.J <drexler2004@icloud.com>
2022-10-02 15:19:47 +0200

    test functionality

--- test/Names.ts
+++ test/Names.ts
@@ -4,13 +4,50 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Names", function () {  
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
+  describe("Functionality", function () {
+    it("Should correctly handle names", async function () {
+      const [owner, other] = await ethers.getSigners();
+
       const Names = await ethers.getContractFactory("Names");
       const names = await Names.deploy();
 
-      //expect().to.equal(true);
-      // test claim function, everything else can be expected to work
+      const sig = owner.signMessage(
+        ethers.utils.arrayify(
+          ethers.utils.solidityKeccak256(
+            ["string"],
+            ["I am worthy."]
+          )
+        )
+      );
+
+      // when testing deactivate the EOA-check
+      
+      // test basic name claiming
+      expect(await names.claim("FirstTest", [])).to.emit(names, "NameClaimed");
+
+      expect(await names.resolveAddress(owner.address)).to.equal("FirstTest/0");
+      
+      // test name overwriting
+      await expect(names.claim("SecondTest", [])).to.be.reverted;
+      expect(await names.claim("ThirdTest", [], {value: ethers.utils.parseEther("5")}));
+      
+      expect(await names.resolveAddress(owner.address)).to.equal("ThirdTest/0");
+
+      // test name overwriting the second time
+      expect(await names.claim("FirstTest", sig, {value: ethers.utils.parseEther("5")})).to.emit(names, "NameClaimed");
+
+      expect(await names.resolveAddress(owner.address)).to.equal("FirstTest/1");
+
+      // test name resolving - every once claimed name should remember it's owner
+      expect(await names.resolveName("FirstTest/0")).to.equal(owner.address);
+      expect(await names.resolveName("ThirdTest/0")).to.equal(owner.address);
+      expect(await names.resolveName("FirstTest/1")).to.equal(owner.address);
+
+      // test resolve unclaimed
+      expect(await names.resolveName("SecondTest/0")).to.equal(ethers.constants.AddressZero);
+
+      // test other signer on already claimed name
+      expect(await names.connect(other).claim("FirstTest", [])).to.emit(names, "NameClaimed").withArgs(other.address, "FirstTest/2");
     });
   });
 });
